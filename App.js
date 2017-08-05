665965aea3cbbe832f37c812e7a1b45492da0f8c
Tristan Southwell <tristan.southwell@hotmail.com>
2017-08-05 11:31:26 +1000

    auth flow working

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 import AuthScreen from './screens/AuthScreen';
 import BuddyListScreen from './screens/BuddyListScreen';
@@ -13,7 +16,7 @@ import WelcomeScreen from './screens/WelcomeScreen';
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
-      welcome: { screen: WelcomeScreen },
+      // welcome: { screen: WelcomeScreen },
       auth: { screen: AuthScreen },
       main: {
         screen: TabNavigator({
@@ -31,9 +34,11 @@ export default class App extends React.Component {
     });
 
     return (
-      <View style={styles.container}>
-        <MainNavigator />
-      </View>
+      <Provider store={ store }>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
